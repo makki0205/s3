86c393ba91139a4949ab5d4f77ab361619935006
s-otsuka <shota_otsuka@voyagegroup.com>
2019-07-07 20:16:37 +0900

    add get pre signed url

--- s3.go
+++ s3.go
@@ -81,7 +81,7 @@ func (f *S3) UpLoad(LocalPath string, S3Path string) error {
 func (f *S3) GetPreSignedUrl(S3Path string, expire time.Duration) (string, error) {
 	req, _ := f.svc.GetObjectRequest(&s3.GetObjectInput{
 		Bucket: aws.String(f.BucketName),
-		Key:    aws.String("S3Path"),
+		Key:    aws.String(S3Path),
 	})
 	url, err := req.Presign(expire)
 	if err != nil {
